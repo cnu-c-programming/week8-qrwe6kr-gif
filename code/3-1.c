2fb95f50e35e794ebb0faaec80ad251358a415c0
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-04-24 10:01:40 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -6,19 +6,19 @@ int main()
     char *names[] = {"apple", "mango", "ant", "banana", "cat", "anaconda"};
     int n = sizeof(names) / sizeof(names[0]);
     
-    for (int i=0; i<n-1; i++) {
-      for (int j = 0; j < n-1-i; j++) {
-          if (strcmp(names[j], names[j + 1]) > 0) { 
-              char temp[50];
-              strcpy(temp, names[j]);               
-              strcpy(names[j], names[j + 1]);
-              strcpy(names[j + 1], temp);
-          }
-       }
-   }
- 
-   for (int i=0; i<n; i++) {
-       printf("%s\n", names[i]);
-   }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (strcmp(names[j], names[j + 1]) > 0) {
+                char *temp = names[j];
+                names[j] = names[j + 1];
+                names[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", names[i]);
+    }
+
     return 0;
 }
