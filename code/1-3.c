e936c30b0cad8f8c2677d982a1f8a77a2783c391
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-04-24 09:17:45 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+
+char *my_strcat(char *dst, const char *src) {
+    char *x = dst;
+    
+    while (*dst != '\0') {
+        dst++;
+    }
+
+    while (*src != '\0') {
+        *dst = *src;
+        dst++;
+        src++;
+    }
+
+    *dst = '\0';
+
+    return x;
+}
+
+int main()
+{
+    char str1[20] = "Hello";
+    char str2[] = "World";
+    
+    printf("%s\n", str1);
+    my_strcat(str1,str2);
+    printf("%d\n", str1);
+    
+    return 0;
+}
+
+
+
+
